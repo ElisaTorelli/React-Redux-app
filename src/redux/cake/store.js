b2b1e635e87a0decd4d586fff9b35641bdbddec7
ElisaTorelli <elisa.torelli1@gmail.com>
2022-07-14 09:52:42 +0200

    add code snippet

--- src/redux/cake/store.js
+++ src/redux/cake/store.js
@@ -2,8 +2,9 @@ import {createStore, applyMiddleware} from 'redux'
 import rootReducer from '../rootReducer'
 // import default logger (after having installed it)
 import logger from 'redux-logger'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-const store = createStore(rootReducer, applyMiddleware(logger))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
 
 export default store
 
